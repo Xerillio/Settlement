dce0b2cda0667a80efb594ad2024457a9f277033
Xerillio <cas0493@gmail.com>
2014-03-29 17:31:40 +0100

    Placeable building scripted
    You can now spawn a cube (building) with 'Q' and move it around with the
    mouse, while it stays in the same height as the ground object (layer:
    Terrain)

---
+++ Settlement/Assets/Scripts/Building.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 placement = this.MouseTerrainPoint();
+		this.transform.position = placement.Equals(new Vector3(-1, -1, -1)) ? this.transform.position : placement;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 placement = this.MouseTerrainPoint();
+		this.transform.position = placement.Equals(new Vector3(-1, -1, -1)) ? this.transform.position : placement;
+	}
+
+	private Vector3 MouseTerrainPoint() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		int mask = 1 << LayerMask.NameToLayer("Terrain");
+		if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, mask)){
+			return hitInfo.point;
+		}
+		return new Vector3(-1, -1, -1);
+	}
+}
---
+++ Settlement/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Transform building;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Q)){
+			Instantiate(building, new Vector3(0,0,0), Quaternion.identity);
+		}
+
+	}
+}
